1feb9d20af4ff9e6fc13caec8c5bbe573cd64944
grego <andrepetridis@gmail.com>
2018-03-08 20:18:41 -0300

    Adicionando alterações na main para testes do Clan

--- src/main/scala/com/brauproject/main.scala
+++ src/main/scala/com/brauproject/main.scala
@@ -1,7 +1,7 @@
 package com.brauproject
 
 import akka.actor.{ActorSystem, Props}
-import com.brauproject.core.{HeroActor, Skill, Task}
+import com.brauproject.core.{HeroActor, Skill, Task, ClanActor, Clan}
 
 object main extends App {
 
@@ -9,6 +9,12 @@ object main extends App {
 
   // Supervising with Fail
   val dude = system.actorOf(HeroActor.getHero("1234"))
+  dude ! Skill("skill_1", "python")
+  dude ! Task("Nice Task", "Do Something in python", Seq(Skill("skill_1", "python")))
+  dude ! Task("Bad Task", "This task you don't know", Seq(Skill("skill_1", "python"), Skill("skill_2", "javascript")))
+
+  // Supervising with Fail
+  val clan_dude = system.actorOf(ClanActor.getClan("1234"))
   dude ! Skill("skill_1", "python")
   dude ! Task("Nice Task", "Do Something in python", Seq(Skill("skill_1", "python")))
   dude ! Task("Bad Task", "This task you don't know", Seq(Skill("skill_1", "python"), Skill("skill_2", "javascript")))
